730d3fe85285713a76d540ba6c07e8054940956f
Nikita <nikich340@gmail.com>
2015-08-30 19:03:59 +1000

    Turn back lib-imsdlp.so

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ TARGET_BOOTLOADER_BOARD_NAME := g2m
 # Kernel image
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/g2m/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/lge/msm8226
+TARGET_KERNEL_SOURCE := kernel/lge/msm8x26
 TARGET_KERNEL_CONFIG := g2mini_cm_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 msm_rtb.filter=0x37 androidboot.hardware=g2m androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
