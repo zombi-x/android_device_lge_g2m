2f7f3c86aec1b233aeebbba593f28dbe906b7239
MobiusM <danholandi@gmail.com>
2015-10-02 20:12:59 +0300

    Update cm.mk
    cleaning some code for omni

--- cm.mk
+++ cm.mk
@@ -1,13 +1,10 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
 # Release name
 PRODUCT_RELEASE_NAME := LG G2 Mini
-PRODUCT_NAME := cm_g2m
+PRODUCT_NAME := omni_g2m
 PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := g2m
 
