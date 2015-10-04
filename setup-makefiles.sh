38109e16f196722e912ffb35b015e34e915ee8b1
Joshua <jl52741@gmail.com>
2015-10-04 12:40:06 -0500

    zombi bringup continues...

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -170,15 +170,15 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
-include \$(CLEAR_VARS)
-LOCAL_MODULE := webview
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/app/webview/webview.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-include \$(BUILD_PREBUILT)
+# include \$(CLEAR_VARS)
+# LOCAL_MODULE := webview
+# LOCAL_MODULE_OWNER := $VENDOR
+# LOCAL_SRC_FILES := proprietary/app/webview/webview.apk
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+# LOCAL_MODULE_CLASS := APPS
+# LOCAL_CERTIFICATE := platform
+# include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
